394d20c04b67d8d7e6a9fe43a0db22a64afc2888
Shubham Jangle <jangleshubham13@gmail.com>
2023-07-16 14:48:26 +0530

    UI improvements

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,22 +1,59 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Unstable_Grid2";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import MediaCard from "@/components/MediaCard";
 
 export default function TasksPage() {
   return (
     <Container>
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Typography variant="body1" gutterBottom>
           Tasks Page
         </Typography>
+        <Box>
+          <Alert severity="info" sx={{ mt: 2, mb: 5 }}>
+            <AlertTitle>Hello 👋</AlertTitle>
+            This app uses the Next.js App Router and Material UI v5.
+          </Alert>
+
+          <Grid container rowSpacing={3} columnSpacing={3}>
+            <Grid xs={6}>
+              <MediaCard
+                heading="CMYK"
+                text="The CMYK color model (also known as process color, or four color) is a subtractive color model, based on the CMY color model, used in color printing, and is also used to describe the printing process itself."
+              />
+            </Grid>
+            <Grid xs={6}>
+              <MediaCard
+                heading="HSL and HSV"
+                text="HSL (for hue, saturation, lightness) and HSV (for hue, saturation, value; also known as HSB, for hue, saturation, brightness) are alternative representations of the RGB color model, designed in the 1970s by computer graphics researchers."
+              />
+            </Grid>
+            <Grid xs={6}>
+              <MediaCard
+                heading="RGB"
+                text="An RGB color space is any additive color space based on the RGB color model. RGB color spaces are commonly found describing the input signal to display devices such as television screens and computer monitors."
+              />
+            </Grid>
+            <Grid xs={6}>
+              <MediaCard
+                heading="CIELAB"
+                text="The CIELAB color space, also referred to as L*a*b*, was intended as a perceptually uniform space, where a given numerical change corresponds to a similar perceived change in color."
+              />
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
     </Container>
   );
